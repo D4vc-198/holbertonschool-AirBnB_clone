f5ad3ffc74b5f136d0772b061a88944b802719a2
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-24 10:46:59 -0600

    pep8 doc

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,18 @@
     Amenity modules
 """
 from models.base_model import BaseModel
+
+
 class Amenity(BaseModel):
+    """
+        class Amenity inherit from base
+        Attribute:
+            name (str)
+    """
     name = ""
+
     def __init__(self, *args, **kwargs):
+        """
+            str
+        """
         super().__init__(*args, **kwargs)

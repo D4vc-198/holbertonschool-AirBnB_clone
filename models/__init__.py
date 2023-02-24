ff241055aa279a85593c8f7973c6bdc32f470554
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-24 10:11:29 -0600

    User, review, city, amenity, place, state in __init__}

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-""" Storage """
+""" __init__ module """
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
+from .user import User
+from .review import Review
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .state import State
 
 storage = FileStorage()
 storage.reload()

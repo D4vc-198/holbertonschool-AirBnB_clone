eef77b5f05cf6257a3bd7f61a93d73f038818714
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-24 10:45:45 -0600

    fix saved

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" __init__ module """
+"""
+    __init__ modules
+"""
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
 from .user import User
